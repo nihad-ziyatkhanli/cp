2b0329881217ade8a86534267a7e33a50024f87a
nihad <name>
2022-10-11 17:23:12 +0400

    Tailwind elements is installed.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ module.exports = {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './src/**/*.{html,js}',
+        './node_modules/tw-elements/dist/js/**/*.js'
     ],
 
     theme: {
@@ -16,5 +18,8 @@ module.exports = {
         },
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('tw-elements/dist/plugin')
+    ],
 };

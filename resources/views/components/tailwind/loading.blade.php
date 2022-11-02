e25ad7abe08d42ee0ec320806b9bc6d2d8006f1f
nihad <name>
2022-11-02 19:16:16 +0400

    .

--- resources/views/components/tailwind/loading.blade.php
+++ resources/views/components/tailwind/loading.blade.php
@@ -1,7 +1,9 @@
-<div class="fixed inset-0 flex justify-center items-center">
-    <div class="fixed inset-0 bg-gray-800 opacity-10">
-    </div>
-    <div class="spinner-border animate-spin inline-block w-10 h-10 border-4 rounded-full text-blue-600">
-        <span class="visually-hidden">Loading...</span>
+<div {{ $attributes }} class="fixed inset-0 z-50">
+    <div class="h-full flex justify-center items-center">
+        <div class="absolute inset-0 bg-gray-800 opacity-10">
+        </div>
+        <div class="spinner-border animate-spin inline-block w-10 h-10 border-4 rounded-full text-blue-600">
+            <span class="visually-hidden">Loading...</span>
+        </div>
     </div>
 </div>
--- resources/views/cp/roles/update.blade.php
+++ resources/views/cp/roles/update.blade.php
@@ -11,9 +11,7 @@
         </div>
     </header>
 
-    <div wire:loading class="fixed z-50">
-        <x-tailwind.loading/>
-    </div>
+    <x-tailwind.loading wire:loading />
 
     <div class="py-2 max-w-7xl mx-auto sm:px-1 lg:px-2">
 
@@ -54,7 +52,7 @@
                             <a data-mdb-ripple="true" data-mdb-ripple-color="light" href="{{ route($mi_code) }}" class="inline-block px-6 py-2.5 bg-gray-200 text-gray-700 font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-300 hover:shadow-lg focus:bg-gray-300 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-400 active:shadow-lg transition duration-150 ease-in-out">
                                 {{ __('Cancel') }}
                             </a>
-                            <button wire:loading.attr="disabled" type="submit" data-mdb-ripple="true" data-mdb-ripple-color="light"
+                            <button type="submit" data-mdb-ripple="true" data-mdb-ripple-color="light"
                             class="inline-block px-6 py-2.5 bg-green-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-green-600 hover:shadow-lg focus:bg-green-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-700 active:shadow-lg transition duration-150 ease-in-out">
                                 {{ __('Save') }}
                             </button>
